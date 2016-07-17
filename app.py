d188278f113c39c2f33f325bd7b5f444204f5ffc
Giulio Calzolari <giulio.calzolari@cloudreach.co.uk>
2016-07-17 19:34:56 +0200

    general update

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ import re
 import csv
 import base64
 import urlparse
+from collections import OrderedDict
 from urllib import urlretrieve
 from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
     FileTransferSpeed, FormatLabel, Percentage, \
@@ -48,76 +49,62 @@ class Solarmovie(LoggingApp):
 
 
     def main(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (compatible, MSIE 11, "\
+        "Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 
         self.log.info("Starting")
-        ep_list = []
+        ep_list = OrderedDict()
         ep_titles = []
         s = requests.session()
         idx = 0
-        r = requests.get(self.params.solarmovie_base+self.params.solarmovie_url)
+        r = requests.get(self.params.solarmovie_base+self.params.solarmovie_url,verify=False, headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
-        for tag in soup.findAll('a', href=True):
+        
+        # print soup.findAll('a', href=True)
+         
+        for tag in soup.select('.seasonEpisodeList .epname a'):
+            if tag['href'] not in ep_list.keys():
+                ep_list[tag['href']] = tag.contents[0].strip()
+        
 
-            next_ep = False
-            idx += 1
+        for ep in reversed(ep_list.keys()):
+        
+            url_ep = self.params.solarmovie_base+ep
 
-            url_ep = self.params.solarmovie_base+tag['href']
+            print ep_list[ep]+" : "+url_ep;
 
-            if "episode" not in tag['href']:
-                continue
-
-            try:
-                url_ep.split("/")[6] 
-            except IndexError:
-                continue
-
-            if url_ep.split("/")[6] in ep_list:
-                continue    
-            ep_title = tag.contents[0].strip()
-
-            if "Episode" in ep_title or "links" in ep_title:
-                continue
-            # print ep_title+"#"
-            r1 = requests.get(self.params.solarmovie_base+tag['href'])
+            r1 = requests.get(url_ep,verify=False, headers=headers)
             soup1 = BeautifulSoup(r1.text, 'html.parser')
-            for ep in soup1.find_all('a', href=True):
+            for ep_link in soup1.select('.movie-table .js-link a'):
 
-                if next_ep:
-                    break
+                if ep_link['href'].startswith("/link/show"):
 
-                if ep['href'].startswith("/link/show"):
-
-                    if ep.contents[0].strip().startswith("vodlocker"):
+                    if ep_link.contents[0].strip().startswith("vodlocker"):
                         # print ep['href']
-                        # print ep.contents[0].strip()
+                        # print self.params.solarmovie_base+ep_link['href']
 
-                        r2 = s.get(self.params.solarmovie_base+ep['href'])
+
+                        r2 = s.get(self.params.solarmovie_base+ep_link['href'],verify=False, headers=headers)
                         soup2 = BeautifulSoup(r2.text, 'html.parser')
                         for final_video in soup2.findAll('a', {'class': 'fullyGreenButton'}, href=True):
                             # print "Final video: "+final_video['href']
-                            r3 = s.get(final_video['href'])
+                            r3 = s.get(final_video['href'],verify=False, headers=headers)
                             soup3 = BeautifulSoup(r3.text)
-                            r4 = s.get(soup3.find('iframe')["src"])
+                            if soup3.find('iframe') == None:
+                                r4 = s.get(soup3.select('.js-play-iframe-container a')[0]['href'],verify=False, headers=headers)
+                            else:
+                                r4 = s.get(soup3.find('iframe')["src"],verify=False, headers=headers)
+
                             p = re.compile(ur'file:\s"(.*)"')
                             rs = re.search(p, r4.text)
                             if rs:
                                 vodlocker_mp4 = rs.group(1)
-                                video_title = "%s_%s_%s.mp4" % (url_ep.split("/")[5], url_ep.split("/")[6], ep_title)
-                            
-                                if video_title in ep_titles:
-                                    continue
-
-                                # stupid file url
-                                if video_title.endswith("                    (.mp4"):
-                                    continue    
-
-
-                                ep_titles.append(video_title)    
+                                video_title = "%s_%s_%s.mp4" % (ep.split("/")[3], ep.split("/")[4], ep_list[ep])
+                              
                                 if os.path.isfile(video_title):
                                     continue
 
-                                # print video_title
-                                # print vodlocker_mp4
+                                print video_title
 
                                 try:
                                     widgets = [ video_title+': ',Percentage(), ' ', Bar(marker=RotatingMarker()), ' ', ETA(), ' ', FileTransferSpeed()]
@@ -128,7 +115,7 @@ class Solarmovie(LoggingApp):
                                     pass
 
 
-                                next_ep = True
+
 
 
 if __name__ == "__main__":
@@ -138,6 +125,10 @@ if __name__ == "__main__":
     app.add_param( "--solarmovie_base", help="solarmovie_base",
                  default="https://www.solarmovie.ph", required=False, action="store")   
     app.add_param("-u","--solarmovie_url", help="solarmovie_url ex. /tv/friends-1994/season-5/",
-                  required=True, action="store")                     
-    app.run()
+                  required=True, action="store")     
+    try:                
+        app.run()
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        sys.exit(0)
 
